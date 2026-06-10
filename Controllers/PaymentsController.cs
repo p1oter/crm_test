d0746cfa0fd8978e41e3de50fdba8433bcd1629d
Piotr Duszyński <piter.duszynski@gmail.com>
2026-06-10 18:14:18 +0200

    dodanie tabeli płatności

---
+++ Controllers/PaymentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CRM.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRM.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private readonly CrmDbContext _context;
+
+        public PaymentsController(CrmDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Payments
+        public async Task<IActionResult> Index()
+        {
+            var payments = await _context.Payments
+                .Include(p => p.BillInfo)
+                .ThenInclude(b => b.Client)
+                .OrderByDescending(p => p.CreatedT)
+                .ToListAsync();
+            return View(payments);
+        }
+    }
+}
